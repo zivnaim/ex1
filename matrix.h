6f2e30db98d608087c120a3de97a827234da91f2
ziv naim <you@example.com>
2020-08-09 08:04:32 +0300

    add functions

---
+++ matrix.h
@@ -0,0 +1,115 @@
+#pragma once
+#define __MATRIX_H
+#include <stdint.h>
+#include <stdio.h>
+#include "../../Downloads/ErrorCode.h"
+#include "ErrorCode.h"
+
+// The implementation of the struct is up to you
+typedef struct Matrix* PMatrix;
+typedef const struct Matrix* CPMatrix;
+
+/**
+ * @brief Creates a new matrix of a given height an width,
+ *  all values are initially zeroes.
+ *
+ * @param[out] matrix The address of a matrix pointer to receive
+ *  the address of the created matrix.
+ * @param[in] height Height of the matrix
+ * @param[in] width Width of the matrix
+ * @return ErrorCode
+ */
+ErrorCode matrix_create(PMatrix* matrix, uint32_t height, uint32_t width);
+
+/**
+ * @brief Creates a new matrix from an old matrix.
+ *
+ * @param[out] matrix The address of a matrix pointer to receive
+ *  the address of the copied matrix.
+ * @param[in] source The matrix to copy.
+ * @return ErrorCode
+ */
+ErrorCode matrix_copy(PMatrix* result, CPMatrix source);
+
+/**
+ * @brief Destroys a matrix.
+ *
+ * @param matrix the matrix to destroy.
+ */
+void matrix_destroy(PMatrix matrix);
+
+/**
+ * @brief Returns the height of a give matrix.
+ *
+ * @param[in] matrix The matrix.
+ * @param[out] result On output, contains the height of the matrix.
+ * @return ErrorCode
+ */
+ErrorCode matrix_getHeight(CPMatrix matrix, uint32_t* result);
+
+/**
+ * @brief Returns the width of a give matrix.
+ *
+ * @param[in] matrix The matrix.
+ * @param[out] result On output, contains the height of the matrix.
+ * @return ErrorCode
+ */
+ErrorCode matrix_getWidth(CPMatrix matrix, uint32_t* result);
+
+/**
+ * @brief Sets a value to the matrix.
+ *
+ * @param[in, out] matrix The matrix to operate on.
+ * @param[in] rowIndex Row index of the value to set.
+ * @param[in] colIndex Column index of the value to setF.
+ * @param[in] value Value to set.
+ * @return ErrorCode
+ */
+ErrorCode matrix_setValue(PMatrix matrix, uint32_t rowIndex, uint32_t colIndex,
+                          double value);
+
+/**
+ * @brief Sets a value to the matrix.
+ *
+ * @param[in] matrix The matrix to operate on.
+ * @param[in] rowIndex Row index of the value to get.
+ * @param[in] colIndex Column index of the value to get.
+ * @param[out] value The address of a double variable to receive
+ *  the value from the matrix.
+ * @return ErrorCode
+ */
+ErrorCode matrix_getValue(CPMatrix matrix, uint32_t rowIndex, uint32_t colIndex,
+                          double* value);
+
+/**
+ * @brief Computes the addition of two matrices.
+ *
+ * @param[out] result The address of a matrix pointer to receive
+ *  the address of the resulting matrix.
+ * @param[in] lhs The left hand side of the addition operation.
+ * @param[in] rhs The right hand side of the addition operation.
+ * @return ErrorCode
+ */
+ErrorCode matrix_add(PMatrix* result, CPMatrix lhs, CPMatrix rhs);
+
+/**
+ * @brief Computes the multiplication of two matrices.
+ *
+ * @param[out] result The address of a matrix pointer to receive
+ *  the address of the resulting matrix.
+ * @param[in] lhs The left hand side of the multiplication operation.
+ * @param[in] rhs The right hand side of the multiplication operation.
+ * @return ErrorCode
+ */
+ErrorCode matrix_multiplyMatrices(PMatrix* result, CPMatrix lhs, CPMatrix rhs);
+
+/**
+ * @brief Multiplies a matrix with a scalar and stores the result in
+ *  the given matrix.
+ *
+ * @param[in, out] matrix On input, the matrix to multiply with a scalar.
+ *  On output, the result of the multiplication operation.
+ * @param[in] scalar The scalar to multiply with.
+ * @return ErrorCode
+ */
+ErrorCode matrix_multiplyWithScalar(PMatrix matrix, double scalar);
