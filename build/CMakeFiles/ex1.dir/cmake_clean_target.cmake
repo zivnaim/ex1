7e4394a8702dda4278bd182549d0c3b8724aa62c
ziv naim <you@example.com>
2020-08-14 09:00:32 +0300

    delete build files and add change to matrix add function

--- build/CMakeFiles/ex1.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "libex1.a"
-)
--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/zivnaim/ass1/ex1
-# Build directory: /home/zivnaim/ass1/ex1/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
